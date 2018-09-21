ebd78474b542456fd04a5b44158abeafaf195c22
joegabonito <joejung5678@gmail.com>
2018-09-21 19:12:18 -0400

    first commit

---
+++ src/main/java/joe/MacroTest.java
@@ -0,0 +1,40 @@
+package joe;
+
+import java.awt.AWTException;
+import java.awt.GraphicsDevice;
+import java.awt.Robot;
+
+import com.sun.glass.events.KeyEvent;
+
+public class MacroTest{
+
+	  Robot robot = new Robot();
+
+	  public static void main(String[] args) throws AWTException {
+	    new MacroTest();
+	  }
+	  
+	  public MacroTest() throws AWTException {
+		  robot.setAutoDelay(40);
+		  robot.setAutoWaitForIdle(true);
+		  for(int i = 0; i < 10; i++) {
+		  type("Hello, world");
+		  robot.delay(40);
+		  robot.keyPress(KeyEvent.VK_ENTER);
+		  robot.keyRelease(KeyEvent.VK_ENTER);
+		  }
+	  }
+	  
+	  private void type(String s) {
+	    byte[] bytes = s.getBytes();
+	    for (byte b : bytes)
+	    {
+	      int code = b;
+	      // keycode only handles [A-Z] (which is ASCII decimal [65-90])
+	      if (code > 96 && code < 123) code = code - 32;
+	      robot.delay(40);
+	      robot.keyPress(code);
+	      robot.keyRelease(code);
+	    }
+	  }
+}
